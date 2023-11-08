24e4f411e079e60d8d5fb46fec8275696c578a85
LauraOlivera07 <laura.olivera.7e7@itb.cat>
2023-11-08 10:01:44 +0100

    a

--- Laura/e5.py
+++ Laura/e5.py
@@ -4,8 +4,19 @@ El programa ha de demanar una data, dia, mes i any (en el format DD/MM/AAAA).
 Cal recordar que hi ha anys de traspàs
 """
 data= input("Introdueix una data (DD/MM/AAAA): ").split("/")
-dia=data[0]
-mes=data[1]
-any=data[2]
+dia=int(data[0])
+mes=int(data[1])
+any=int(data[2])
 
-print(any)
+if mes>12 or dia>31 or any<0:
+    print("Data incorrecta")
+
+elif mes==4 or mes==6 or mes==9 or mes==11:
+    if dia>30:
+        print("Data incorrecta")
+
+elif mes==2:
+    if any%100==0 and any%400!=0 and dia>=29 or dia>=30:
+        print("Data incorrecta")
+else:
+    print("Data correcta")
