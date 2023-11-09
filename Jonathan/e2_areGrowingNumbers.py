c6e460723d9ffd2704cb161f121a132988401aa2
jonathan.merce.7e7 <jonathan.merce.7e7@itb.cat>
2023-11-09 13:00:31 +0100

    ok

--- Jonathan/e2_areGrowingNumbers.py
+++ Jonathan/e2_areGrowingNumbers.py
@@ -9,13 +9,13 @@ num1 = int(input("Quin és el primer número?"))
 num2 = int(input("Quin és el segon número?"))
 num3 = int(input("Quin és el tercer número?"))
 
-if num1 >= num2 and num2 >= num3:
+if num1 >= num2 >= num3:
         print(f"{num1, num2, num3}")
 
-elif num2 >= num1 and num1 >= num3:
+elif num2 >= num1 >= num3:
         print(f"{num2, num1, num3}")
 
-elif num3 >= num1 and num1 >= num2:
+elif num3 >= num1 >= num2:
         print(f"{num2, num1, num3}")
 
 # elif(num2 <= num1 <= num3:
